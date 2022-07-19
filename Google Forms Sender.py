e8aa78ee36f984c93fbb57bd9d2da85cdfce9917
sh3pard dev <105732620+sh3pardDEV@users.noreply.github.com>
2022-07-19 12:08:31 +0300

    0.2

--- Google Forms Sender.py
+++ Google Forms Sender.py
@@ -1,4 +1,5 @@
 import requests;
+import time;
 requestsSend = 0
 DocsURL = "https://docs.google.com/forms/d/e/1FAIpQLSdocbCFAVO11dsiPJ9knKgAx3_fc5zvpUqQc6WB4MF3QtR9XA"
 send = 0
@@ -9,10 +10,18 @@ if requestsValue > 400:
     print("Ошибка. Такое количество анкет нельзя указать (могут быть ограничения от Google)")
 else:
     print("OK.")
+    startTime = time.time()
 while send != requestsValue:
     send+=1
     requestsSend-=1
     form_data = {'entry.119283706': 'test request'}
     print(f"Отправлено {send} запросов")
     user_agent = {'Referer':urlReferer,'User-Agent': "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.52 Safari/537.36"}
-    r = requests.post(urlResponse, data=form_data, headers=user_agent) 
+    r = requests.post(urlResponse, data=form_data, headers=user_agent)
+if send == requestsValue:
+    endTime = time.time()
+    allTime = int(endTime - startTime)
+    print(f"Работа заняла {allTime} секунд")
+
+
+# TODO: Добавить количество времени потраченное на работу. Выводить в конце.
